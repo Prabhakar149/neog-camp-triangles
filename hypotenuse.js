3940f4503ec00c07ee8b6e3c013851d5003386d4
Prabhakar Singh <prabhakarsingh10798@gmail.com>
2022-12-02 10:31:15 +0530

    fix: updated validation

--- hypotenuse.js
+++ hypotenuse.js
@@ -7,11 +7,15 @@ function sumOfSquares(a, b) {
 }
 function calculateHypotenuse() {
   if (hypotenuseInputs[0].value && hypotenuseInputs[1].value) {
-    const sum = sumOfSquares(Number(hypotenuseInputs[0].value),Number     (hypotenuseInputs [1].value));
-    const hypotenuse = Math.sqrt(sum);
-    hypotenuseMessage.innerText = "hypotenuse of triangle: " + hypotenuse;
+    if(hypotenuseInputs[0].value<0 || hypotenuseInputs[1].value<0){
+      hypotenuseMessage.innerText = "the base or height of triangle can not be negative value";
+    }else{
+      const sum = sumOfSquares(Number(hypotenuseInputs[0].value),Number     (hypotenuseInputs [1].value));
+      const hypotenuse = Math.sqrt(sum).toFixed(3);
+      hypotenuseMessage.innerText = `hypotenuse of triangle is ${hypotenuse} units`;
+    }  
   }else{    
-    hypotenuseMessage.innerText = "please give the value of base and height of triangle"
+    hypotenuseMessage.innerText = "please give the value of base and height of triangle";
   }
 }
 hypotenuseButton.addEventListener("click", calculateHypotenuse);
